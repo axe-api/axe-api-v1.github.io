da6f903f8e93491e3d4b2fefa49578cd621e7f1d
ozziest <ozziest@users.noreply.github.com>
2023-10-25 20:10:16 +0000

    deploy: axe-api/docs@a4f9ef20f49dc30668a87306dd446831148c15c1

---
+++ assets/reference_api-configs.md.d1b1cec6.js
@@ -0,0 +1,47 @@
+import{_ as s,o as n,c as a,Q as p}from"./chunks/framework.b8d6d8db.js";const h=JSON.parse('{"title":"API Configs","description":"","frontmatter":{},"headers":[],"relativePath":"reference/api-configs.md","filePath":"reference/api-configs.md"}'),l={name:"reference/api-configs.md"},o=p(`<h1 id="api-configs" tabindex="-1">API Configs <a class="header-anchor" href="#api-configs" aria-label="Permalink to &quot;API Configs&quot;">​</a></h1><div class="vp-code-group vp-adaptive-theme"><div class="tabs"><input type="radio" name="group-ImKko" id="tab-ABwfwLb" checked="checked"><label for="tab-ABwfwLb">app/config.ts</label></div><div class="blocks"><div class="language-ts vp-adaptive-theme active"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> { IApplicationConfig } </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;axe-api&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F97583;">const</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">config</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">IApplicationConfig</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  prefix: </span><span style="color:#9ECBFF;">&quot;api&quot;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">  env: p<wbr>rocess.env.</span><span style="color:#79B8FF;">NODE_ENV</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">||</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;production&quot;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">  port: p<wbr>rocess.env.</span><span style="color:#79B8FF;">APP_PORT</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">?</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">parseInt</span><span style="color:#E1E4E8;">(p<wbr>rocess.env.</span><span style="color:#79B8FF;">APP_PORT</span><span style="color:#E1E4E8;">) </span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">3000</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">  pino: {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">...</span></span>
+<span class="line"><span style="color:#E1E4E8;">  },</span></span>
+<span class="line"><span style="color:#E1E4E8;">  rateLimit: {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">...</span></span>
+<span class="line"><span style="color:#E1E4E8;">  },</span></span>
+<span class="line"><span style="color:#E1E4E8;">  database: {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">...</span></span>
+<span class="line"><span style="color:#E1E4E8;">  },</span></span>
+<span class="line"><span style="color:#E1E4E8;">  cache: {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">...</span></span>
+<span class="line"><span style="color:#E1E4E8;">  },</span></span>
+<span class="line"><span style="color:#E1E4E8;">  redis: {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">...</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">};</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F97583;">export</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">default</span><span style="color:#E1E4E8;"> config;</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> { IApplicationConfig } </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;axe-api&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#D73A49;">const</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">config</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">IApplicationConfig</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">  prefix: </span><span style="color:#032F62;">&quot;api&quot;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">  env: p<wbr>rocess.env.</span><span style="color:#005CC5;">NODE_ENV</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">||</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;production&quot;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">  port: p<wbr>rocess.env.</span><span style="color:#005CC5;">APP_PORT</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">?</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">parseInt</span><span style="color:#24292E;">(p<wbr>rocess.env.</span><span style="color:#005CC5;">APP_PORT</span><span style="color:#24292E;">) </span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">3000</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">  pino: {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">...</span></span>
+<span class="line"><span style="color:#24292E;">  },</span></span>
+<span class="line"><span style="color:#24292E;">  rateLimit: {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">...</span></span>
+<span class="line"><span style="color:#24292E;">  },</span></span>
+<span class="line"><span style="color:#24292E;">  database: {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">...</span></span>
+<span class="line"><span style="color:#24292E;">  },</span></span>
+<span class="line"><span style="color:#24292E;">  cache: {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">...</span></span>
+<span class="line"><span style="color:#24292E;">  },</span></span>
+<span class="line"><span style="color:#24292E;">  redis: {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">...</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">};</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#D73A49;">export</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">default</span><span style="color:#24292E;"> config;</span></span></code></pre></div></div></div><h2 id="prefix" tabindex="-1"><code>prefix</code> <a class="header-anchor" href="#prefix" aria-label="Permalink to &quot;\`prefix\`&quot;">​</a></h2><p><code>prefix</code> is the default API prefix that will be used in all routes.</p><div class="language-bash vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#B392F0;">GET</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">/api/v1/users/:userId/posts/:id</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6F42C1;">GET</span><span style="color:#24292E;"> </span><span style="color:#032F62;">/api/v1/users/:userId/posts/:id</span></span></code></pre></div><p>Suggestions;</p><ul><li>It should be a string</li><li>Do not use numbers</li><li>You are allowed to keep it empty.</li></ul><h2 id="env" tabindex="-1"><code>env</code> <a class="header-anchor" href="#env" aria-label="Permalink to &quot;\`env\`&quot;">​</a></h2><p><code>env</code> means which environment your API will work.</p><p>Some examples; are <code>development</code>, <code>testing</code>, <code>staging</code>, and <code>production</code>.</p><p>Suggestions;</p><ul><li>Using environment variables is suggested</li><li>You SHOULD use <code>production</code> for the production environment to protect sensitive data.</li></ul><h2 id="port" tabindex="-1"><code>port</code> <a class="header-anchor" href="#port" aria-label="Permalink to &quot;\`port\`&quot;">​</a></h2><p><code>port</code> means in which port the application will run. You can select any port that is available for your setup.</p>`,14),e=[o];function c(t,r,i,E,y,d){return n(),a("div",null,e)}const f=s(l,[["render",c]]);export{h as __pageData,f as default};
