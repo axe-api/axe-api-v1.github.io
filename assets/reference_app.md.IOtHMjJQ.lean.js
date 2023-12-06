c40ade7ca222958999b7a4c719119a46d10eb76f
ozziest <ozziest@users.noreply.github.com>
2023-12-06 15:42:09 +0000

    deploy: axe-api/docs@cabc697a3edd2ca4d27e496fa8059178fbf1e2aa

---
+++ assets/reference_app.md.IOtHMjJQ.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,R as n}from"./chunks/framework.Kvze0q-R.js";const F=JSON.parse('{"title":"App","description":"","frontmatter":{},"headers":[],"relativePath":"reference/app.md","filePath":"reference/app.md"}'),p={name:"reference/app.md"},h=n("",22),k=[h];function t(l,e,r,d,E,g){return a(),i("div",null,k)}const o=s(p,[["render",t]]);export{F as __pageData,o as default};
