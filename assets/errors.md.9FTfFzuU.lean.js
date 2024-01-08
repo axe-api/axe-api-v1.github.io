6848b682df4b9b30aa431fbf41319939a05c4f5d
ozziest <ozziest@users.noreply.github.com>
2024-01-08 17:13:19 +0000

    deploy: axe-api/docs@62c1d351b1a10cc5a021d4254649967f3d2ec037

--- assets/errors.md.9FTfFzuU.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o as s,R as i}from"./chunks/framework.BGZWni3-.js";const k=JSON.parse('{"title":"Errors","description":"","frontmatter":{},"headers":[],"relativePath":"errors.md","filePath":"errors.md"}'),o={name:"errors.md"},n=i("",35),l=[n];function t(d,r,h,c,p,E){return s(),a("div",null,l)}const m=e(o,[["render",t]]);export{k as __pageData,m as default};
