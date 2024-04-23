b2547b52ce80ea1ce44b5b36faa52db77352f23c
ozziest <ozziest@users.noreply.github.com>
2024-04-23 19:34:13 +0000

    deploy: axe-api/docs@42edd72541b5b2f30b178cbc88e90d6f6916f278

---
+++ assets/ecosystem_faq.md.B1LCUTHR.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o,a9 as r}from"./chunks/framework.CdMsPAd6.js";const m=JSON.parse('{"title":"FAQ","description":"","frontmatter":{},"headers":[],"relativePath":"ecosystem/faq.md","filePath":"ecosystem/faq.md"}'),t={name:"ecosystem/faq.md"},i=r("",41),s=[i];function n(l,u,h,d,p,c){return o(),a("div",null,s)}const w=e(t,[["render",n]]);export{m as __pageData,w as default};
